6f48bfa37cd8c2fcd4ee3cd1408ad39212c997d4
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2024-10-14 06:41:25 +0500

    fetch every in 1 second

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,7 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-    },
-  },
-});
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
